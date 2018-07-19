abba3dc5ce0187bb3cc38b1c024b22192f983f5e
maureensayshi <maureen81711@gmail.com>
2018-07-19 03:43:30 -0700

    update

--- week-3/Assignment_1/app.jsx
+++ week-3/Assignment_1/app.jsx
@@ -15,66 +15,64 @@ Column.propTypes = {
 
 function Container(props) {
   return (
-    <div className="container clearfix">
-      <header>
-        <div className="top">
-          <h2>Section Title</h2>
-        </div>
-      </header>
-      <Column boxcss="one col" boxtitle="content box 1" boxcontent="content box 1content box 1content box 1content box 1" />
-      <Column boxcss="two col" boxtitle="content box 2" boxcontent="content box 1content box 1content box 1content box 1" />
-      <Column boxcss="three col" boxtitle="content box 3" boxcontent="content box 1content box 1content box 1content box 1" />
-      <Column boxcss="four col" boxtitle="content box 4" boxcontent="content box 1content box 1content box 1content box 1" />
-    </div> //.container
-
-    <div className="bottom">
-          Call-to-Action
-    </div> //.bottom
-
-    <div className="hidden">
-      <div className="container">
+    <div>
+      <div className="container clearfix">
+        <header>
+          <div className="top">
+            <h2>Section Title</h2>
+          </div>
+        </header>
         <Column boxcss="one col" boxtitle="content box 1" boxcontent="content box 1content box 1content box 1content box 1" />
         <Column boxcss="two col" boxtitle="content box 2" boxcontent="content box 1content box 1content box 1content box 1" />
         <Column boxcss="three col" boxtitle="content box 3" boxcontent="content box 1content box 1content box 1content box 1" />
         <Column boxcss="four col" boxtitle="content box 4" boxcontent="content box 1content box 1content box 1content box 1" />
+      </div> //.container
+
+      <div className="bottom">
+            Call-to-Action
+      </div> //.bottom
+
+      <div className="hidden">
+        <div className="container">
+          <Column boxcss="one col" boxtitle="content box 1" boxcontent="content box 1content box 1content box 1content box 1" />
+          <Column boxcss="two col" boxtitle="content box 2" boxcontent="content box 1content box 1content box 1content box 1" />
+          <Column boxcss="three col" boxtitle="content box 3" boxcontent="content box 1content box 1content box 1content box 1" />
+          <Column boxcss="four col" boxtitle="content box 4" boxcontent="content box 1content box 1content box 1content box 1" />
+        </div>
       </div>
     </div>
   );
 }
-
-Container.propTypes = {
-
-};
+//
+// Container.propTypes = {
+//
+// };
 
 function Application(props) {
   return (
-    <header className="main-header">
-      <h1 className="name"><a href="#">web title</a></h1>
-      <ul className="main-nav">
-        <li><a href="#">item1</a></li>
-        <li><a href="#">item2</a></li>
-        <li><a href="#">item3</a></li>
-        <li><a href="#">item4</a></li>
-      </ul>
-      <img id="image" className="main_nav_replacer" src="img/menu.png"/>
-      <ul className="Menu">
-        <li><a href="#">item1</a></li>
-        <li><a href="#">item2</a></li>
-        <li><a href="#">item3</a></li>
-        <li><a href="#">item4</a></li>
-      </ul>
-    </header>
-    <div className="slogan">
-      <h1 id="change">Welcome Message</h1>
-    </div><!--/.slogan-container-->
-
-    <Container />
-
+    <div>
+      <header className="main-header">
+        <h1 className="name"><a href="#">web title</a></h1>
+        <ul className="main-nav">
+          <li><a href="#">item1</a></li>
+          <li><a href="#">item2</a></li>
+          <li><a href="#">item3</a></li>
+          <li><a href="#">item4</a></li>
+        </ul>
+        <img id="image" className="main_nav_replacer" src="img/menu.png"/>
+        <ul className="Menu">
+          <li><a href="#">item1</a></li>
+          <li><a href="#">item2</a></li>
+          <li><a href="#">item3</a></li>
+          <li><a href="#">item4</a></li>
+        </ul>
+      </header>
+      <div className="slogan">
+        <h1 id="change">Welcome Message</h1>
+      </div>
+      <Container />
+    </div>
   );
 }
-
-Application.propTypes = {
-
-};
 
 ReactDOM.render(<Application />, document.getElementById('webpage'));
