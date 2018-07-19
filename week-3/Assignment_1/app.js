51a1fb8a992e16ead59959dd0cc002cc7b71b5c3
maureensayshi <maureen81711@gmail.com>
2018-07-18 19:25:42 -0700

    update

---
+++ week-3/Assignment_1/app.js
@@ -0,0 +1,50 @@
+var hidden = document.querySelector(".hidden");
+var bottom = document.querySelector(".bottom");
+var Menu = document.querySelector(".Menu");
+var image = document.getElementById('image');
+
+const change = document.getElementById('change');
+change.addEventListener ( 'click', ()=> {
+  change.textContent = "Have a Good Time!";
+});
+
+
+bottom.addEventListener('click', ()=> {
+  if(hidden.style.display == "" || hidden.style.display == "none"){
+    hidden.style.display = "block";
+  } else {
+    hidden.style.display = "none";
+  }
+});
+
+image.addEventListener('click', ()=> {
+  if(Menu.style.display == "" || Menu.style.display == "none"){
+    Menu.style.display = "block";
+    image.src = "./img/x.png";
+  } else {
+    Menu.style.display = "none";
+    image.src = "./img/menu.png";
+  }
+});
+
+
+/* const callfor = document.getElementById('callfor');
+const hidden = document.getElementById('hidden');
+
+callfor.addEventListener('click', () => {
+    hidden();
+});
+
+function hidden(){
+
+if(hidden.style.display == "none"){
+
+hidden.style.display = "block";
+} else {
+
+hidden.style.display == "none";
+};
+}
+
+
+  // change.textContent = "Noooop"; */
---
+++ week-3/Assignment_1/app.jsx
@@ -0,0 +1,83 @@
+function Column(props){
+  return (
+    <div className={props.boxcss}>
+      <h3>{props.boxtitle}</h3>
+      <p>{props.boxcontent}</p>
+    </div>
+  );
+}
+
+Column.propTypes = {
+  boxcss: React.PropTypes.string.isRequired,
+  boxtitle: React.PropTypes.string.isRequired,
+  boxcontent: React.PropTypes.string.isRequired,
+};
+
+function Container(props) {
+  return (
+    <div className="container clearfix">
+      <header>
+        <div className="top">
+          <h2>Section Title</h2>
+        </div>
+      </header>
+      <Column boxcss="one col" boxtitle="content box 1" boxcontent="content box 1content box 1content box 1content box 1" />
+      <Column boxcss="two col" boxtitle="content box 2" boxcontent="content box 1content box 1content box 1content box 1" />
+      <Column boxcss="three col" boxtitle="content box 3" boxcontent="content box 1content box 1content box 1content box 1" />
+      <Column boxcss="four col" boxtitle="content box 4" boxcontent="content box 1content box 1content box 1content box 1" />
+    </div> //.container
+
+    <div className="bottom">
+    </div>
+    <div className="bottom">
+          Call-to-Action
+    </div> //.bottom
+    <div className="hidden">
+      <div className="container">
+        <Column boxcss="one col" boxtitle="content box 1" boxcontent="content box 1content box 1content box 1content box 1" />
+        <Column boxcss="two col" boxtitle="content box 2" boxcontent="content box 1content box 1content box 1content box 1" />
+        <Column boxcss="three col" boxtitle="content box 3" boxcontent="content box 1content box 1content box 1content box 1" />
+        <Column boxcss="four col" boxtitle="content box 4" boxcontent="content box 1content box 1content box 1content box 1" />
+     </div>//<!--/.container-->
+    </div>//<!--/.hidden--
+  );
+}
+
+Container.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.num.isRequired,
+};
+
+function Application(props) {
+  return (
+    <header className="main-header">
+      <h1 className="name"><a href="#">web title</a></h1>
+      <ul className="main-nav">
+        <li><a href="#">item1</a></li>
+        <li><a href="#">item2</a></li>
+        <li><a href="#">item3</a></li>
+        <li><a href="#">item4</a></li>
+      </ul>
+      <img id="image" className="main_nav_replacer" src="img/menu.png"/>
+      <ul className="Menu">
+        <li><a href="#">item1</a></li>
+        <li><a href="#">item2</a></li>
+        <li><a href="#">item3</a></li>
+        <li><a href="#">item4</a></li>
+      </ul>
+    </header>
+    <div className="slogan">
+      <h1 id="change">Welcome Message</h1>
+    </div><!--/.slogan-container-->
+
+    <Container />
+
+  );
+}
+
+Application.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.num.isRequired,
+};
+
+ReactDOM.render(<Application />, document.getElementById('webpage'));
